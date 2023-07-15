1a3c2938a5c1331d841e9a6a5c7dcb7dccef391a
pengoosedev <73521518+pengoosedev@users.noreply.github.com>
2023-07-16 05:28:24 +0900

    feat: add theme interface

---
+++ style/src/theme/types/theme.ts
@@ -0,0 +1,23 @@
+export interface Theme {
+  color: {
+    primary: string;
+    secondary: string;
+    accentColor: string;
+    default: string;
+    light: string;
+    transparent: string;
+    border: string;
+  };
+  background: {
+    default: string;
+    primary: string;
+    secondary: string;
+    tertiary: string;
+    quaternary: string;
+    quinary: string;
+    transparent: string;
+    hover: string;
+  };
+  transitionOption: string;
+  boxShadow: string;
+}
