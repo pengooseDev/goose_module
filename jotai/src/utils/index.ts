b3d5aca4e60812ecebeeb1c882f5c5c15911a32e
Pengoose <pengoose_dev@naver.com>
2024-02-17 19:28:37 +0900

    feat: add useManager

--- jotai/src/utils/index.ts
+++ jotai/src/utils/index.ts
@@ -1,2 +1,1 @@
-export * from './atomManager';
-export * from './strictAtomManager';
+export * from './useManager';
---
+++ jotai/src/utils/useManager.ts
@@ -0,0 +1,27 @@
+import { useAtomValue, useSetAtom } from 'jotai';
+import { ActionValues, Actions, SelectorValues, Selectors } from 'types';
+
+export const useManager = <T>(atomManager: {
+  selectors: Selectors<T>;
+  actions: Actions<T>;
+}) => {
+  const selectors = Object.entries(atomManager.selectors).reduce(
+    (acc, [key, selectorAtom]) => {
+      acc[key as keyof T] = useAtomValue(selectorAtom);
+
+      return acc;
+    },
+    {} as SelectorValues<T>
+  );
+
+  const actions = Object.entries(atomManager.actions).reduce(
+    (acc, [key, actionAtom]) => {
+      acc[key] = useSetAtom(actionAtom);
+
+      return acc;
+    },
+    {} as ActionValues
+  );
+
+  return { ...selectors, ...actions };
+};
