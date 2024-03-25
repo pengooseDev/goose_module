a8867329c7efbe3f41d0088130c475b8b83c29f7
pengoosedev <73521518+pengoosedev@users.noreply.github.com>
2024-03-25 11:34:41 +0900

    fix: hook 호출 순서 보장을 위한 커링 적용

--- jotai/src/utils/useManager.ts
+++ jotai/src/utils/useManager.ts
@@ -1,21 +1,45 @@
 import { Atom, WritableAtom, useAtomValue, useSetAtom } from 'jotai';
 import { AtomManager } from '../manager';
 
-// type ExtractSelectors<T> = {
-//   [P in keyof T]: T[P] extends Atom<infer V> ? V : never;
-// };
+/**
+ * @description
+ * - wraps useAtomValue in atomManager's selectors and returns it.
+ * @param {Atom<T>} atom
+ * @returns
+ */
+const createUseSelector = <T>(atom: Atom<T>) => {
+  return () => useAtomValue(atom);
+};
 
-// type ExtractActions<T> = {
-//   [P in keyof T]: T[P] extends WritableAtom<any, infer U, void>
-//     ? (param: U[0]) => void
-//     : never;
-// };
+/**
+ * @description
+ * - wraps useSetAtom in atomManager's actions and returns it.
+ * @param {WritableAtom<T, any, void>} atom
+ * @returns
+ */
+const createUseAction = <T>(atom: WritableAtom<T, any, void>) => {
+  return () => useSetAtom(atom);
+};
 
+/**
+ * @description
+ * - wraps useAtomValue in atomManager's selectors and returns it.
+ * - wraps useSetAtom in atomManager's actions and returns it.
+ * - infers the type of each Atom and returns it.
+ *
+ * @param manager
+ * @returns
+ */
 export const useManager = <T extends AtomManager<any>>(manager: T) => {
+  /**
+   * @description
+   * - wraps useAtomValue in atomManager's selectors and returns it.
+   * - infers the type of each Atom and returns it.
+   */
   const selectors = Object.fromEntries(
     Object.entries(manager.selectors).map(([key, atom]) => [
       key,
-      useAtomValue(atom),
+      createUseSelector(atom)(),
     ])
   ) as {
     [P in keyof T['selectors']]: T['selectors'][P] extends Atom<infer V>
@@ -23,10 +47,15 @@ export const useManager = <T extends AtomManager<any>>(manager: T) => {
       : never;
   };
 
+  /**
+   * @description
+   * - wraps useSetAtom in atomManager's actions and returns it.
+   * - infers the type of each Atom and returns it.
+   */
   const actions = Object.fromEntries(
     Object.entries(manager.actions).map(([key, actionAtom]) => [
       key,
-      useSetAtom(actionAtom),
+      createUseAction(actionAtom)(),
     ])
   ) as {
     [P in keyof T['actions']]: T['actions'][P] extends WritableAtom<
@@ -40,3 +69,13 @@ export const useManager = <T extends AtomManager<any>>(manager: T) => {
 
   return { selectors, actions };
 };
+
+// type ExtractSelectors<T> = {
+//   [P in keyof T]: T[P] extends Atom<infer V> ? V : never;
+// };
+
+// type ExtractActions<T> = {
+//   [P in keyof T]: T[P] extends WritableAtom<any, infer U, void>
+//     ? (param: U[0]) => void
+//     : never;
+// };
