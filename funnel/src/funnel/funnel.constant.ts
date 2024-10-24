0c6b8c904f37e7aa9a431af810524f71cd6b259a
Pengoose <pengoose_dev@naver.com>
2024-10-24 11:15:07 +0900

    chore(@pengoose/funnel): improve Error message

--- funnel/src/funnel/funnel.constant.ts
+++ funnel/src/funnel/funnel.constant.ts
@@ -1,6 +1,6 @@
 const ERROR = {
-  STEP_NOT_INITIALIZED: 'Please Initialize setStep function.',
-  DATA_NOT_INITIALIZED: 'Please Initialize setData function.',
+  STEP_NOT_INITIALIZED: 'Please use setStep in FunnelContext.',
+  DATA_NOT_INITIALIZED: 'Please use setData in FunnelContext.',
 } as const;
 
 const MESSAGE = { ERROR } as const;
