e9ffc3a865422b7660de63df339d989c5784ee8e
pengoosedev <73521518+pengoosedev@users.noreply.github.com>
2025-03-05 22:52:09 +0900

    Initial commit

---
+++ mission-utils/src/console.ts
@@ -0,0 +1,67 @@
+import readline from 'readline';
+
+interface Props {
+  readLine: {
+    query: string;
+    callback: (answer: string) => void;
+  };
+}
+class Console {
+  constructor() {}
+
+  static readLine(
+    query: Props['readLine']['query'],
+    callback: Props['readLine']['callback']
+  ) {
+    if (arguments.length !== 2) {
+      throw new Error('arguments must be 2.');
+    }
+
+    if (typeof query !== 'string') {
+      throw new Error('query must be string');
+    }
+
+    if (typeof callback !== 'function') {
+      throw new Error('callback must be function');
+    }
+
+    if (callback.length !== 1) {
+      throw new Error('callback must have 1 argument');
+    }
+
+    const rl = readline.createInterface({
+      input: process.stdin,
+      output: process.stdout,
+    });
+
+    rl.question(query, callback);
+  }
+
+  static readLineAsync(query: string) {
+    return new Promise((resolve, reject) => {
+      if (arguments.length !== 1) {
+        reject(new Error('arguments must be 1'));
+      }
+
+      if (typeof query !== 'string') {
+        reject(new Error('query must be string'));
+      }
+
+      const rl = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout,
+      });
+
+      rl.question(query, (input) => {
+        rl.close();
+        resolve(input);
+      });
+    });
+  }
+
+  static print(message: string) {
+    console.log(message);
+  }
+}
+
+export default Console;
---
+++ mission-utils/src/random.ts
@@ -0,0 +1,132 @@
+interface Props {
+  Range: {
+    startInclusive: number;
+    endInclusive: number;
+  };
+  pickNumberInList: {
+    array: number[];
+  };
+}
+
+class Random {
+  constructor() {}
+
+  static pickNumberInRange(
+    startInclusive: Props['Range']['startInclusive'],
+    endInclusive: Props['Range']['endInclusive']
+  ) {
+    Random.#validateRange(startInclusive, endInclusive);
+
+    startInclusive = Math.ceil(startInclusive);
+
+    return (
+      Math.floor(Math.random() * (endInclusive + 1 - startInclusive)) +
+      startInclusive
+    );
+  }
+
+  static #isNumber(value: any) {
+    return typeof value === 'number';
+  }
+
+  static #validateRange(
+    startInclusive: Props['Range']['startInclusive'],
+    endInclusive: Props['Range']['endInclusive']
+  ) {
+    if (!Random.#isNumber(startInclusive) || !Random.#isNumber(endInclusive)) {
+      throw new Error('arguments must be numbers.');
+    }
+
+    if (startInclusive < Number.MIN_SAFE_INTEGER) {
+      throw new Error(
+        'startInclusive cannot be less than Number.MIN_SAFE_INTEGER'
+      );
+    }
+
+    if (endInclusive > Number.MAX_SAFE_INTEGER) {
+      throw new Error(
+        'endInclusive cannot be greater than Number.MAX_SAFE_INTEGER.'
+      );
+    }
+
+    if (startInclusive > endInclusive) {
+      throw new Error(
+        `startInclusive ${startInclusive} cannot be greater than endInclusive ${endInclusive}.`
+      );
+    }
+
+    if (endInclusive - startInclusive >= Number.MAX_VALUE) {
+      throw new Error('the input range is too large.');
+    }
+  }
+
+  static pickNumberInList(array: Props['pickNumberInList']['array']) {
+    Random.#validateEmptyArray(array);
+
+    return array[Random.pickNumberInRange(0, array.length - 1)];
+  }
+
+  static #validateEmptyArray(array: any[]) {
+    if (!Array.isArray(array)) {
+      throw new Error('the argument must be an array.');
+    }
+
+    if (!array.every((v) => Random.#isNumber(v))) {
+      throw new Error('array elements must be numbers.');
+    }
+
+    if (array.length === 0) {
+      throw new Error('argument array cannot be empty.');
+    }
+  }
+
+  static pickUniqueNumbersInRange(
+    startInclusive: Props['Range']['startInclusive'],
+    endInclusive: Props['Range']['endInclusive'],
+    count: number
+  ) {
+    Random.#validateIntsRange(startInclusive, endInclusive, count);
+
+    const result = [];
+
+    for (let i = startInclusive; i <= endInclusive; i++) {
+      result.push(i);
+    }
+
+    return Random.shuffle(result).slice(0, count);
+  }
+
+  static #validateIntsRange(
+    startInclusive: Props['Range']['startInclusive'],
+    endInclusive: Props['Range']['endInclusive'],
+    count: number
+  ) {
+    if (
+      !Random.#isNumber(startInclusive) ||
+      !Random.#isNumber(endInclusive) ||
+      !Random.#isNumber(count)
+    ) {
+      throw new Error('arguments must be numbers.');
+    }
+
+    if (count < 0) {
+      throw new Error('count cannot be less than zero.');
+    }
+
+    if (endInclusive - startInclusive + 1 < count) {
+      throw new Error(
+        `count: ${count} cannot be greater than the input range (endInclusive - startInclusive): ${
+          endInclusive - startInclusive
+        }.`
+      );
+    }
+  }
+
+  static shuffle(array: any[]) {
+    Random.#validateEmptyArray(array);
+
+    return array.sort(() => Math.random() - 0.5);
+  }
+}
+
+export default Random;
